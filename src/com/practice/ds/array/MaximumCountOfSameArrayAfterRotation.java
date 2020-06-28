3398b7ec9063340978b265d3ad1c938e7e0bc9df
Your Name <you@example.com>
2020-06-28 10:47:09 +0530

    Code

---
+++ src/com/practice/ds/array/MaximumCountOfSameArrayAfterRotation.java
@@ -0,0 +1,14 @@
+package com.practice.ds.array;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MaximumCountOfSameArrayAfterRotation {
+    public static void main(String[] args) {
+
+    }
+
+    private static void maxCountOfSameElement(int[] array){
+        Map<Integer,Integer> map = new HashMap<>();
+    }
+}
